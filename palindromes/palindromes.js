98d2420377f71bbf0ab0202deae9896f648bd981
Stephany Camacho-Lara <stephany_c19@yahoo.com>
2020-05-15 00:53:53 -0600

    Palindrome assignment complete

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function() {
-
+const palindromes = function (str) {
+    const origWord = (str) => str.toLowerCase().replace(/[\W_]/g, '')
+    const cleanStr = origWord(str);
+    const reverseStr = cleanStr.split('').reverse().join('');
+    return reverseStr === cleanStr
 }
 
 module.exports = palindromes
+
+// explain how regex works
