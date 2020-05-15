9b5e529f787a89dad45a8b45c3fc3730d05cc281
Stephany Camacho-Lara <stephany_c19@yahoo.com>
2020-05-15 01:24:58 -0600

    Fibonacci complete

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let arr = [0, 1];
+    for (let i = 2; i < num + 1; i++){
+      arr.push(arr[i - 2] + arr[i -1])
+    }
+    if (num <= 0) return 'OOPS';
+   return arr[num]
 }
 
 module.exports = fibonacci
